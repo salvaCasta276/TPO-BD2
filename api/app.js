1324f39f0de559b168300c0817c63600657bc916
AdolfoRosauer1 <jurosauer@itba.edu.ar>
2023-09-29 10:01:18 -0300

    test implementation

--- api/app.js
+++ api/app.js
@@ -6,13 +6,6 @@ const port = 3000;
 
 app.use(express.json()); // Para poder manejar JSON en el body
 
-// Swagger
-// const swaggerUi = require('swagger-ui-express');
-// const YAML = require('yamljs');
-// const swaggerDocument = YAML.load('./swagger.yaml');
-
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 // Rutas para manejar clientes
 app.get('/clientes', async (req, res) => {
     try {
@@ -26,6 +19,21 @@ app.get('/clientes', async (req, res) => {
       res.send("Error " + err);
     }
   });
+
+app.get('/clientes/:id', async (req, res) => {
+const { id } = req.params;
+try {
+    const result = await pool.query('SELECT * FROM E01_CLIENTE WHERE nro_cliente = $1', [id]);
+    if (result.rows.length > 0) {
+    res.status(200).json(result.rows[0]);
+    } else {
+    res.status(404).json({ error: 'Cliente no encontrado' });
+    }
+} catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al recuperar cliente' });
+}
+});
 
 app.post('/clientes', async (req, res) => {
     const { nombre, apellido, direccion, activo } = req.body;
@@ -48,11 +56,16 @@ app.put('/clientes/:id', async (req, res) => {
     const { nombre, apellido, direccion, activo } = req.body;
   
     try {
-      await pool.query(
-        `UPDATE E01_CLIENTE SET nombre = $1, apellido = $2, direccion = $3, activo = $4 WHERE nro_cliente = $5`,
-        [nombre, apellido, direccion, activo, id]
-      );
-      res.status(200).json({ message: 'Cliente actualizado exitosamente' });
+        const result = await pool.query(
+            `UPDATE E01_CLIENTE SET nombre = $1, apellido = $2, direccion = $3, activo = $4 WHERE nro_cliente = $5 RETURNING nro_cliente`,
+            [nombre, apellido, direccion, activo, id]
+          );
+          
+          if (result.rows.length > 0) {
+            res.status(200).json({ message: 'Cliente actualizado exitosamente' });
+          } else {
+            res.status(404).json({ error: 'Cliente no encontrado' });
+          }          
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: 'Error al actualizar cliente' });
@@ -64,8 +77,13 @@ app.delete('/clientes/:id', async (req, res) => {
     const id = req.params.id;
   
     try {
-      await pool.query(`DELETE FROM E01_CLIENTE WHERE nro_cliente = $1`, [id]);
-      res.status(200).json({ message: 'Cliente eliminado exitosamente' });
+        const result = await pool.query(`DELETE FROM E01_CLIENTE WHERE nro_cliente = $1 RETURNING nro_cliente`, [id]);
+
+        if (result.rows.length > 0) {
+          res.status(200).json({ message: 'Cliente eliminado exitosamente' });
+        } else {
+          res.status(404).json({ error: 'Cliente no encontrado' });
+        }        
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: 'Error al eliminar cliente' });
@@ -88,6 +106,21 @@ app.post('/productos', async (req, res) => {
       res.status(500).json({ error: 'Error al crear nuevo producto' });
     }
   });
+
+app.get('/productos/:id', async (req, res) => {
+const { id } = req.params;
+try {
+    const result = await pool.query('SELECT * FROM E01_PRODUCTO WHERE codigo_producto = $1', [id]);
+    if (result.rows.length > 0) {
+    res.status(200).json(result.rows[0]);
+    } else {
+    res.status(404).json({ error: 'Producto no encontrado' });
+    }
+} catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al recuperar producto' });
+}
+});
   
 
 app.put('/productos/:id', async (req, res) => {
@@ -95,18 +128,41 @@ app.put('/productos/:id', async (req, res) => {
     const { marca, nombre, descripcion, precio, stock } = req.body;
   
     try {
-      await pool.query(
-        `UPDATE E01_PRODUCTO SET marca = $1, nombre = $2, descripcion = $3, precio = $4, stock = $5 WHERE codigo_producto = $6`,
-        [marca, nombre, descripcion, precio, stock, id]
-      );
-      res.status(200).json({ message: 'Producto actualizado exitosamente' });
+        const result = await pool.query(
+            `UPDATE E01_PRODUCTO SET marca = $1, nombre = $2, descripcion = $3, precio = $4, stock = $5 WHERE codigo_producto = $6 RETURNING codigo_producto`,
+            [marca, nombre, descripcion, precio, stock, id]
+          );
+          
+          if (result.rows.length > 0) {
+            res.status(200).json({ message: 'Producto actualizado exitosamente' });
+          } else {
+            res.status(404).json({ error: 'Producto no encontrado' });
+          }          
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: 'Error al actualizar producto' });
     }
   });
+
+app.delete('/productos/:id', async (req, res) => {
+const { id } = req.params;
+try {
+    const result = await pool.query('DELETE FROM E01_PRODUCTO WHERE codigo_producto = $1 RETURNING *', [id]);
+    if (result.rows.length > 0) {
+    res.status(200).json({ message: 'Producto eliminado con éxito' });
+    } else {
+    res.status(404).json({ error: 'Producto no encontrado' });
+    }
+} catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al eliminar producto' });
+}
+});
   
 
 app.listen(port, () => {
   console.log(`API corriendo en http://localhost:${port}`);
 });
+
+
+module.exports = app;
